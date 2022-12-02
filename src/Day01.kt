819257b0cfdabe5ccc7c85d6ce2422c7e0bcf5ff
rossilor95 <rossilor95@outlook.it>
2022-12-02 12:17:57 +0100

    feat: Day01, add solution to part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,25 +2,25 @@ import java.io.File
 
 fun main() {
     fun part1(input: String): Int {
-        val data = input.split("\n\n").map { elf -> elf.split("\n").map { calory -> calory.toInt() } }
+        val data = input.split("\n\n").map { elf ->
+                elf.split("\n").map { calory -> calory.toInt() }
+            }
         return data.maxOf { it.sum() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = File("src/Day01_test.txt").readText()
-    check(part1(testInput) == 24000)
+    fun part2(input: String): Int {
+        val data = input.split("\n\n").map { elf ->
+                elf.split("\n").map { calory -> calory.toInt() }
+            }.map { it.sum() }
+        return data.sortedDescending().subList(0, 3).sum()
+    }
 
+    val testInput = File("src/Day01_test.txt").readText()
     val input = File("src/Day01.txt").readText()
+
+    check(part1(testInput) == 24000)
     println(part1(input))
 
-//
-//    fun part2(input: List<String>): Int {
-//        return input.size
-//    }
-//
-//
-//    val input = readInput("Day01")
-//    println(part1(input))
-//    println(part2(input))
-
+    check(part2(testInput) == 45000)
+    println(part2(input))
 }
